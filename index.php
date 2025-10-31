5240903858371ae0342fabd46d69691f9a49b57b
Dante Gallina <dante_gallina@hotmail.com>
2025-10-31 15:05:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<title>Softecuestre</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="Softecuestre" />
+<meta name="keywords" content="FEA, FEI, ecuestre, hipismo, jump, salto, endurance, equitacion, software, equs, hipico, caballo, horse, concurso, carrera, competencia, calendario, club, administracion, jurado, resultado, juez, federacion, rider, jinete, argentina" />
+<meta http-equiv="Content-Language" content="ES" />
+<meta name="copyright" content="2023" />
+<meta name="robots" content="ALL" />
+<meta name="revisit-after" content="7 days" />
+<meta name="reply-to" content="info@softecuestre.com.ar" />
+<meta name="document-rating" content="General" />
+<meta name="document-distribution" content="Global" />
+<meta name="cache-control" content="no-cache" />
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/interface.js"></script>
+
+<!--[if lt IE 7]>
+ <style type="text/css">
+ div, images { behavior: url(iepngfix.htc) }
+ </style>
+<![endif]-->
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+<body>
+<?php    
+$trabajando=""; // si
+
+ if($trabajando=="si"){
+            echo "<center><img src='http://softecuestre.com.ar/images/logo_3.jpg' width='150' height='100' /></br></br>";
+            echo "<h2>Estamos trabajando para brindarle un mejor servicio</h2></br></br>";
+            echo "<img src='http://softecuestre.com.ar/images/loading-100.gif' width='150' height='100' /></center>";
+        }else{    
+            echo "<META HTTP-EQUIV='Refresh'";
+            echo "CONTENT='0; URL=https://softecuestre.com.ar/src/concursos.php'>";
+        }
+?>
+</body>
+</html>
---
+++ indexold.php
@@ -0,0 +1,138 @@
+﻿
+<html>
+<head>
+<title>Softecuestre</title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="Softecuestre" />
+<meta name="keywords" content="FEA, FEI, ecuestre, hipismo, jump, salto, endurance, equitacion, software, equs, hipico, caballo, horse, concurso, carrera, competencia, calendario, club, administracion, jurado, resultado, juez, federacion, rider, jinete, argentina" />
+<meta http-equiv="Content-Language" content="ES" />
+<meta name="copyright" content="2023" />
+<meta name="robots" content="ALL" />
+<meta name="revisit-after" content="7 days" />
+<meta name="reply-to" content="info@softecuestre.com.ar" />
+<meta name="document-rating" content="General" />
+<meta name="document-distribution" content="Global" />
+<meta name="cache-control" content="no-cache" />
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/interface.js"></script>
+
+<!--[if lt IE 7]>
+ <style type="text/css">
+ div, images { behavior: url(iepngfix.htc) }
+ </style>
+<![endif]-->
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+  
+<style type="text/css">
+<!--
+body {
+	background-color: #CCCCCC;
+}
+-->
+</style>
+<style type="text/css">
+<!--
+body {
+	background-image: url();
+	background-repeat: repeat;
+	background-color: #000000;
+}
+.Estilo3 {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	color: #666633;
+	font-size: 10px;
+}
+-->
+</style></head>
+
+<body>
+<p>&nbsp;</p>
+<table width="900" height="32" border="0" align="center" cellspacing="0">
+  <tr>
+    <th width="62" height="32" bordercolor="0" bgcolor="#660000" scope="col">&nbsp;</th>
+    <th width="25" bordercolor="0" bgcolor="#660000" scope="col"><images src="images/banda.jpg" alt="1" width="25" height="30" /></th>
+    <th width="717" height="32" bgcolor="#660000" scope="col">&nbsp;</th>
+    <th width="27" bgcolor="#660000" scope="col"><images src="images/banda_2.jpg" alt="2" width="25" height="30" /></th>
+    <th width="59" height="32" bordercolor="0" bgcolor="#660000" scope="col">&nbsp;</th>
+  </tr>
+</table>
+<table width="900" border="0" align="center" cellspacing="0">
+  <tr>
+    <th scope="col"><images src="images/linea.jpg" width="900" height="5" /></th>
+  </tr>
+</table>
+<table width="900" height="520" border="0" align="center" cellspacing="0">
+  <tr>
+    <th valign="top" align="center" width="62" height="400" rowspan="3" bordercolor="0" bgcolor="#660000" scope="col">
+
+
+
+</th>
+    <th width="25" rowspan="3" bordercolor="0" bgcolor="#660000" scope="col"><images src="images/banda.jpg" width="25" height="650" /></th>
+    <th width="718" height="132" bgcolor="#FFFFFF" scope="col">
+    <?PHP
+     include_once("menu/menu.htm"); 
+?>    </th>
+    <th width="26" rowspan="3" bgcolor="#660000" scope="col"><images src="images/banda_2.jpg" width="25" height="650" /></th>
+    <th valign="top" align="center" width="59" height="400" rowspan="3" bordercolor="0" bgcolor="#660000" scope="col">
+
+
+
+
+
+</th>
+  </tr>
+  <tr>
+    <th width="200" height="200" align="center" bgcolor="#FFFFFF" scope="col"><img src="../images/logo_3.jpg" width="200" height="150" /></th>
+  </tr>
+  <tr>
+
+
+    <th width="718" height="157" align="center" bgcolor="#FFFFFF" scope="col"><table width="650" height="49" border="0" align="center" cellspacing="0">
+      <tr>
+
+<th width="15">
+</th>
+
+        <th scope="col" align="left"><span class="Estilo3">Bienvenido a Softecuestre, la primer p&aacute;gina de soluciones on-line para competencias ecuestres de latinoam&eacute;rica. <br>
+Este emprendimiento de HORSE WORLD se basa en la necesidad del sector de brindar soluciones tecnológicas a las competencias y de obtener datos estadísticos fiables e históricos de jinetes y caballos. Con esta idea, profesionales de la informática que poseen mas de 20 años de experiencia como jinetes, jurados y dirigentes de clubes, desarrollaron el primer servicio integral específicamente con estos objetivos.<br>
+Lo invitamos a navegar nuestro sitio y esperamos poder acompañarlo y ayudarlo a mejorar las tareas de los Comités Organizadores de sus competencias así como la organización de su club o haras.<br>
+<br>
+<br>
+El Staff de Soft Ecuestre  
+</span></th>
+<th width="15">
+</th>
+        </tr>
+    </table></th>
+
+
+  </tr>
+</table>
+<table width="900" border="0" align="center" cellspacing="0">
+  <tr>
+    <th height="5" scope="col"><images src="images/linea.jpg" width="900" height="5" /></th>
+  </tr>
+</table>
+<table width="900" height="32" border="0" align="center" cellspacing="0">
+  <tr>
+    <th width="62" height="32" bordercolor="0" bgcolor="#660000" scope="col">&nbsp;</th>
+    <th width="26" bordercolor="0" bgcolor="#660000" scope="col"><images src="images/banda.jpg" width="25" height="30" /></th>
+    <th width="718" height="32" bgcolor="#660000" scope="col">
+    
+    <?PHP
+	                include_once("menu/pie.htm"); 
+            	?>    </th>
+    <th width="25" bgcolor="#660000" scope="col"><images src="images/banda_2.jpg" width="25" height="30" /></th>
+    <th width="59" height="32" bordercolor="0" bgcolor="#660000" scope="col">&nbsp;</th>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
